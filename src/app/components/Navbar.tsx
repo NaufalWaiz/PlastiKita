5a38188ed9aed2586a0dde4cb282779a7813a6cf
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2025-11-24 15:49:04 +0700

    making the navbar fixed and adding shadow to the navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,12 +11,12 @@ export default function Navbar() {
     }
     return (
         <>
-            <div className="absolute flex flex-col gap-1 left-3 top-3 sm:hidden" onClick={activateNavbar}>
+            <div className="flex flex-col gap-1 left-3 top-3 sm:hidden sticky" onClick={activateNavbar}>
                 <div className="w-6 h-1 bg-[#88A825] rounded-full"></div>
                 <div className="w-6 h-1 bg-[#88A825] rounded-full"></div>
                 <div className="w-6 h-1 bg-[#88A825] rounded-full"></div>
             </div>
-            <div className={`${isActive ? 'translate-x-0' : '-translate-x-full'} sm:translate-x-0 z-10 flex w-10/12 h-screen sm:h-auto sm:w-full fixed transition sm:absolute bg-[#88A825] sm:bg-white font-medium text-[21px] gap-14 text-black py-5 flex-col sm:flex-row sm:justify-end px-32 font-poppins`}>
+            <div className={`fixed ${isActive ? 'translate-x-0' : '-translate-x-full'} shadow-2xl sm:translate-x-0 z-10 flex w-10/12 h-screen sm:h-auto sm:w-full fixed transition bg-[#88A825] sm:bg-white font-medium text-[21px] gap-14 text-black py-5 flex-col sm:flex-row sm:justify-end px-32 font-poppins`}>
                 <Link href={"#"}>Beranda</Link>
                 <Link href={"#"}>Tentang</Link>
                 <Link href={"#"}>Artikel</Link>
